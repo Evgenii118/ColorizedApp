c30d7025cd8da453e64aa2fd3446c9ed4771cca6
Ewgeny Izyurov <help@izyapp.ru>
2024-10-03 17:35:56 +0500

    Working with application logic in ViewController

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -15,23 +15,52 @@ final class ViewController: UIViewController {
     @IBOutlet var greenLabel: UILabel!
     @IBOutlet var blueLabel: UILabel!
     
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        setupPaintedView()
     }
-
+    
     // MARK: - IB Action
     @IBAction func redSliderAction() {
+        changeColorPaintedView()
+        redLabel.text = String(format: "%.2f", redSlider.value)
     }
     
     @IBAction func greenSliderAction() {
-        
+        changeColorPaintedView()
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
     }
     
     @IBAction func blueSliderAction() {
-        
+        changeColorPaintedView()
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
     }
     
+    // MARK: Private function
+    private func setupPaintedView() {
+        paintedView.layer.cornerRadius = 15
+        paintedView.backgroundColor = UIColor(
+            red: 0.5,
+            green: 0.5,
+            blue: 0.5,
+            alpha: 1
+        )
+    }
+    
+    private func changeColorPaintedView() {
+        paintedView.backgroundColor = UIColor(
+            cgColor: CGColor(
+                red: CGFloat(redSlider.value),
+                green: CGFloat(greenSlider.value),
+                blue: CGFloat(blueSlider.value),
+                alpha: 1
+            )
+        )
+    }
 }
 
